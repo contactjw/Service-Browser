76c7475130a991f1fd00f89af77ca40de9d98cdb
John West <contactjohnwest@gmail.com>
2020-06-10 17:58:01 -0700

    finished service browser

---
+++ src/ServiceServer.java
@@ -0,0 +1,7 @@
+import java.rmi.*;
+
+public interface ServiceServer extends Remote {
+    Object[] getServiceList() throws RemoteException;
+
+    Service getService(Object serviceKey) throws RemoteException;
+}
